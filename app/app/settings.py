a7ceb642d65cf868d69b152086f1f6ef9dbb5448
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-21 07:21:20 -0300

    build: add databases services data

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*%4omguwgaew39-*tlv$bb(h5k1tin4+-%q)5_cjrw87)wway'
+SECRET_KEY = "django-insecure-#*%4omguwgaew39-*tlv$bb(h5k1tin4+-%q)5_cjrw87)wway"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,52 +32,82 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "core",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {},
+    "auth": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("AUTH_DB_NAME", "auth"),
+        "USER": os.environ.get("AUTH_DB_USER", "user"),
+        "PASSWORD": os.environ.get("AUTH_DB_PASSWORD", "password"),
+        "HOST": os.environ.get("AUTH_DB_HOST", "auth-db"),
+        "PORT": os.environ.get("AUTH_DB_PORT", "5432"),
+    },
+    "users": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("USERS_DB_NAME", "users"),
+        "USER": os.environ.get("USERS_DB_USER", "user"),
+        "PASSWORD": os.environ.get("USERS_DB_PASSWORD", "password"),
+        "HOST": os.environ.get("USERS_DB_HOST", "users-db"),
+        "PORT": os.environ.get("USERS_DB_PORT", "3306"),
+    },
+    "billing": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("BILLING_DB_NAME", "billing"),
+        "USER": os.environ.get("BILLING_DB_USER", "user"),
+        "PASSWORD": os.environ.get("BILLING_DB_PASSWORD", "password"),
+        "HOST": os.environ.get("BILLING_DB_HOST", "billing-db"),
+        "PORT": os.environ.get("BILLING_DB_PORT", "3306"),
+    },
+    "videos": {
+        "ENGINE": "django_mongodb_backend",
+        "NAME": os.environ.get("VIDEOS_DB_NAME", "videos"),
+        "USER": os.environ.get("VIDEOS_DB_USER", "user"),
+        "PASSWORD": os.environ.get("VIDEOS_DB_PASSWORD", "password"),
+        "HOST": os.environ.get("VIDEOS_DB_HOST", "videos-db"),
+        "PORT": os.environ.get("VIDEOS_DB_PORT", 27017),
+    },
 }
 
 
@@ -85,16 +116,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -102,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -114,9 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
